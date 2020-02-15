38d10348d2180a6f4e1c91a2a432b27c54222d91
wzl <il>
2020-02-15 23:11:37 +0800

    这次还不得行就不弄了

---
+++ springcloud_parent/registycenter/src/main/java/com/wzl/registycenter/RegistycenterApplication.java
@@ -0,0 +1,13 @@
+package com.wzl.registycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistycenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistycenterApplication.class, args);
+    }
+
+}
---
+++ springcloud_parent/registycenter/src/test/java/com/wzl/registycenter/RegistycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzl.registycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistycenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
