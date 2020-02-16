4653b5f654cbe17c6b182f90228746a02570b5c3
wzl <il>
2020-02-16 15:40:17 +0800

    wzl/configclient/controller/TestConfig.java

---
+++ springcloud_parent/configclient/src/main/java/com/wzl/configclient/controller/TestConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C), 2020, lywl有限公司
+ * FileName: TestConfig
+ * Author:   lenovo
+ * Date:     2020/2/16 15:32
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.wzl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈〉
+ *
+ * @author lenovo
+ * @create 2020/2/16
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping(value = "/config")
+public class TestConfig {
+    @Value("${server.port}")
+     private int port;
+
+    @GetMapping(value = "/getport")
+    public String getPort(){
+        return "this port == "+port;
+    }
+}
